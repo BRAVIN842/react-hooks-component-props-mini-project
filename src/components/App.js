3ecacb6f2166b2beb2f7371b3df14a6f881a915a
Bravin <onsasebravin853@gmail.com>
2023-11-08 20:21:16 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import blogData from "../data/blog";
-
+import Article from "./Article";
+import Header from "./Header";
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+       <Header name={blogData.name} />
+       <div className="articles">
+        {blogData.posts.map((post) => (
+          <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+          minutes={post.minutes} 
+          />
+        ))}
+       </div>
     </div>
   );
 }
